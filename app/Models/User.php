b4defd6381fefbacd4f2a57da989bb375f6ccdb3
Finn <finn.groenewoud@gmail.com>
2025-08-06 20:58:50 +0200

    Update User model with property annotations

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,13 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Carbon\Carbon $email_verified_at
+ * @property string $password
+ */
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
